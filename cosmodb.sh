d28602b9c92297acb70fe7fded0e9e24bc95a901
Pierre Roman <pierrer@microsoft.com>
2017-05-16 16:03:16 -0700

    sudo

--- cosmodb.sh
+++ cosmodb.sh
@@ -3,11 +3,11 @@
 # install azurecli
 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
+     tee /etc/apt/sources.list.d/azure-cli.list
 
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-sudo apt-get install apt-transport-https
-sudo apt-get update && sudo apt-get install azure-cli
+apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+apt-get install apt-transport-https
+apt-get update && sudo apt-get install azure-cli
 
 # Set variables for the new account, database, and collection
 
