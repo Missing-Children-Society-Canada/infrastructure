ec5c93d60896d174403ebb6e940c02136eb6e462
PatLac04 <patrice.lacroix@outlook.com>
2017-05-18 19:49:14 -0700

    ccc

--- dev-deploy.ps1
+++ dev-deploy.ps1
@@ -7,16 +7,6 @@ echo '*****************************************************'
 
 $resourceGroupName = "MCSC-Dev2-RG"
 $location = "eastus2"
-$COSMOS_DB_ACCOUNTNAME = "missingchildrendata55"
-$SERVICE_BUS_NAMESPACE = "MCSCChildFinderPL"
-$FUNC_APP_SUPPORTING_API_NAME = "PL77-mcsc-supporting-api"
-$FUNC_APP_WEBHOOK_NAME = "PL88-mcsc-webhook"
-$FUNC_APP_CF_FUNCTION_NAME = "PL99-mscs-cf-functions"
-$NAME = ""
-$NAME = ""
-$NAME = ""
-$NAME = ""
-$NAME = ""
 
 # Create a resource group
 az group create --name $resourceGroupName --location $location
@@ -30,3 +20,6 @@ az group deployment create --name DeploySupporting-api --resource-group $resourc
 az group deployment create --name DeployWebHooksFunctionApp --resource-group $resourceGroupName --template-file mcsc-webhook.json --parameters @mcsc-webhook.parameters.json
 
 az group deployment create --name DeployCFFunctionApp --resource-group $resourceGroupName --template-file mscs-cf-functions-v2.json --parameters @mscs-cf-functions-v2.parameters.json
+
+
+
